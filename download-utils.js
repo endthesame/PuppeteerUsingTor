50df721a8dc639db56c38a9ffa52310f4da7357a
eterentiev <endthesame@gmail.com>
2023-11-26 14:40:00 +0300

    setup for iop; fixes download

--- download-utils.js
+++ download-utils.js
@@ -1,11 +1,39 @@
-const path = require('path');
+const puppeteer = require('puppeteer-extra');
+const StealhPlugin = require('puppeteer-extra-plugin-stealth');
 const fs = require('fs');
-const http = require('http');
-const https = require('https');
-const {changeTorIp} = require('./tor-config');
+const path = require('path');
+const { exec } = require('child_process');
+const {changeTorIp, shouldChangeIP} = require('./tor-config');
+
+puppeteer.use(StealhPlugin());
+
 
 async function downloadPDFs(linksFilePath, pdfFolderPath) {
+
+    puppeteer.use(require('puppeteer-extra-plugin-user-preferences')({
+        userPrefs: {
+            download: {
+                prompt_for_download: false,
+                directory_upgrade: true,
+                default_directory:  pdfFolderPath,
+                extensions_to_open: "applications/pdf",
+            },
+            plugins: {
+                always_open_pdf_externally: true,
+                plugins_disabled: ["Chrome PDF Viewer"],
+            },
+        }
+    }));
+
+
+
     const links = fs.readFileSync(linksFilePath, 'utf-8').split('\n');
+
+    let browser = await puppeteer.launch({
+        args: ['--proxy-server=127.0.0.1:8118'],
+        headless: 'new' //'new' for "true mode" and false for "debug mode (Browser open))"
+    });
+    let page = await browser.newPage();
 
     for (const link of links) {
         if (!link.trim()) {
@@ -15,71 +43,73 @@ async function downloadPDFs(linksFilePath, pdfFolderPath) {
         const [pdfLink, pdfFileName] = link.trim().split(' ');
 
         const pdfSavePath = path.join(pdfFolderPath, pdfFileName);
+        const tempDownloadPath = pdfSavePath.slice(0, -4);
 
         try {
-            await downloadPDF(pdfLink, pdfSavePath);
+            await downloadPDF(page, pdfLink, tempDownloadPath);
             console.log(`PDF downloaded successfully from ${pdfLink} and saved as ${pdfSavePath}`);
+            await new Promise(resolve => setTimeout(resolve, 3000));
+            await shouldChangeIP(page)
+            await browser.close()
+            browser = await puppeteer.launch({
+                args: ['--proxy-server=127.0.0.1:8118'],
+                headless: 'new' //'new' for "true mode" and false for "debug mode (Browser open))"
+            });
+            page = await browser.newPage();
+
+            // Получаем список файлов во временной папке
+            // const files = fs.readdirSync(tempDownloadPath);
+            // console.log(`Files found in ${tempDownloadPath}: ${files}`);
+            // // Перемещаем и переименовываем первый найденный файл
+            // if (files.length > 0) {
+            //     const tempFilePath = path.join(tempDownloadPath, files[0]);
+            //     fs.renameSync(tempFilePath, pdfSavePath);
+            //     console.log(`File moved and renamed to ${pdfSavePath}`);
+            // } else {
+            //     console.error(`Error: No files found in ${tempDownloadPath}`);
+            // }
+            // // Удаляем временную папку
+            // fs.rmdirSync(path.dirname(tempDownloadPath), { recursive: true });
+            // console.log(`Temporary folder deleted at ${path.dirname(tempDownloadPath)}`);
+
         } catch (error) {
             console.error(`Error downloading PDF from ${pdfLink}: ${error.message}`);
-            await changeTorIp();
-            await new Promise(resolve => setTimeout(resolve, 15000));
+            await new Promise(resolve => setTimeout(resolve, 10000));
+            //changeTorIp();
+
+            // ИЗ-ЗА ТОГО ЧТО КАЖДОЕ СКАЧИВАНИЕ ФАЙЛА - ВЫДАЕТ ОШИБКУ NET::ERR_ABORTED (ПОТОМУ ЧТО СКАЧИВАТЬ НЕЛЬЗЯ, ТО И ТУТ ЗАДАЕТСЯ РЕНЕЙМ ФАЙЛОВ)
+            // Получаем список файлов во временной папке
+            const files = fs.readdirSync(tempDownloadPath);
+            console.log(`Files found in ${tempDownloadPath}: ${files}`);
+            // Перемещаем и переименовываем первый найденный файл
+            try {
+                if (files.length > 0) {
+                    const tempFilePath = path.join(tempDownloadPath, files[0]);
+                    fs.renameSync(tempFilePath, pdfSavePath);
+                    console.log(`File moved and renamed to ${pdfSavePath}`);
+                } else {
+                    console.error(`Error: No files found in ${tempDownloadPath}`);
+                }
+                // Удаляем временную папку
+                fs.rmdirSync(tempDownloadPath, { recursive: true });
+                console.log(`Temporary folder deleted at ${tempDownloadPath}`);
+
+            } catch (error) {
+                console.log("Cannot rename file and remove dir: ", error)
+            }
         }
     }
+
+    await browser.close();
 }
 
-function downloadPDF(pdfLink, pdfSavePath) {
-    return new Promise((resolve, reject) => {
-        const proxyOptions = {
-            host: 'http://127.0.0.1:8118',
-            //port: 8118,
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-                // 'Accept-Encoding': 'gzip, deflate, br',
-                // 'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-                // 'Connection': 'keep-alive',
-                // 'Origin': 'https://iopscience.iop.org',
-                // 'Referer': 'https://iopscience.iop.org/',
-                // 'Cookie': '__uzma=5b312d6f-5190-4a11-81f0-dce3638b30c7; __uzmb=1695607141; __uzme=2463; __uzmc=1549276027478; __uzmd=1700991659; IOP_session_live=%2F%2F1700991659305%7Cfe56979a-4a2f-4422-a303-80a58367fb6b%7Cefd26e5b-6b6a-427b-aad2-cb2c89aec92c%7C%7C%7C%7C%7C%7C%7C%7C%7Cguest%2Fb322e9fc6e6ae64b7980e4b3b4ef97a2; JSESSIONID=A2D82C9C242ADD61C25EF4ACD8DF390E; AWSALB=GivUHS/Yy/a3qkJBU4G9iDlsEnzm9sgneCZ5QNvZVJCRGHcfQyXXYCMVUzsT9FKI/Am16lNQGMAAKutK5PYkxoTGfG1bxAYL0xF89WfTF30+b1u04OK0L4tKhD/w; AWSALBCORS=GivUHS/Yy/a3qkJBU4G9iDlsEnzm9sgneCZ5QNvZVJCRGHcfQyXXYCMVUzsT9FKI/Am16lNQGMAAKutK5PYkxoTGfG1bxAYL0xF89WfTF30+b1u04OK0L4tKhD/w',
-                // 'Sec-Fetch-Dest': 'document',
-                // 'Sec-Fetch-Mode': 'navigate',
-                // 'Sec-Fetch-Site': 'same-origin',
-                // 'Sec-Fetch-User': '?1'
-            },
-        };
-
-        const downloader = pdfLink.startsWith('https') ? https : http;
-
-        const request = downloader.get(pdfLink, proxyOptions, (response) => {
-            if (response.statusCode >= 300 && response.statusCode < 400 && response.headers.location.includes('perfdrive')) {
-                reject(new Error('Redirecting to a perfdrive URL. Handle accordingly.'));
-                return;
-            }
-            if (response.statusCode >= 400) {
-                reject(new Error(`Error: HTTP status code ${response.statusCode}`));
-                return;
-            }
-
-            const fileStream = fs.createWriteStream(pdfSavePath);
-
-            response.pipe(fileStream);
-
-            fileStream.on('finish', () => {
-                fileStream.close();
-                resolve();
-            });
-        }).on('error', (err) => {
-            fs.unlink(pdfSavePath, () => {});
-            reject(new Error(`Error downloading file: ${err.message}`));
-        });
-
-        request.on('socket', (socket) => {
-            // Настройка прокси
-            // socket.on('connect', () => {
-            //   socket.write(`CONNECT ${proxyOptions.host}:${proxyOptions.port} HTTP/1.1\r\n\r\n`);
-            // });
-        });
+async function downloadPDF(page, pdfLink, tempDownloadPath) {
+    await page._client().send('Page.setDownloadBehavior', {
+        behavior: 'allow',
+        downloadPath: tempDownloadPath
     });
+    await page.goto(pdfLink, { waitUntil: 'networkidle0', timeout: 30000 });
+    await page.waitForTimeout(6000);
 }
 
 module.exports = {downloadPDFs, downloadPDF };
