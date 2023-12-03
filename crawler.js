8f1145c40fc2313c3bac85ea642b25f9445899dc
eterentiev <endthesame@gmail.com>
2023-12-03 19:25:41 +0300

    setup for informs

--- crawler.js
+++ crawler.js
@@ -95,8 +95,8 @@ async function extractData(page, jsonFolderPath, pdfFolderPath, siteFolderPath, 
 
         if (isOpenAccess) {
             pdfLinksToDownload = await page.evaluate(() => {
-                var pdfLinks = Array.from(document.querySelectorAll('.download_transportable > li > a')).find(a => a.querySelector('span')?.textContent.includes('View PDF'))?.getAttribute('href') || '';
-                return "https://pubsonline.informs.org" +  pdfLinks.replace("epdf", "pdf") + "?download=true";
+                var pdfLinks = Array.from(document.querySelectorAll('.download_transportable > a')).find(a => a.querySelector('span')?.textContent.includes('View PDF'))?.getAttribute('href') || '';
+                return "https://pubsonline.informs.org" + pdfLinks.replace("epdf", "pdf") + "?download=true";
                 //"https://pubsonline.informs.org" + 
 
                 // const pdfLinks = Array.from(document.querySelectorAll("a[href]"))
