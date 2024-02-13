1f0e8c44c79e6de413400cd3c2612cc0cdda2049
eterentev <eterentev@tesyan.ru>
2024-02-13 12:44:16 +0300

    pdf fix; added func covert rom to arab numeral

--- crawler.js
+++ crawler.js
@@ -53,8 +53,57 @@ async function extractData(page, jsonFolderPath, pdfFolderPath, siteFolderPath, 
             }
           
             return result.join(";; ");
-          }
-          
+        }
+
+        function romanToNumberOrReturn(input) {
+            const romanNumerals = {
+                'I': 1,
+                'V': 5,
+                'X': 10,
+                'L': 50,
+                'C': 100,
+                'D': 500,
+                'M': 1000,
+                'i': 1,
+                'v': 5,
+                'x': 10,
+                'l': 50,
+                'c': 100,
+                'd': 500,
+                'm': 1000
+
+            };
+        
+            // Проверка, является ли входное значение римской цифрой
+            function isRoman(input) {
+                return /^[IVXLCDMivxlcdm]+$/i.test(input);
+            }
+        
+            // Если входное значение не является римской цифрой, возвращаем его без изменений
+            if (!isRoman(input)) {
+                return input;
+            }
+        
+            let result = 0;
+            let prevValue = 0;
+        
+            // Преобразование римской цифры в число
+            for (let i = input.length - 1; i >= 0; i--) {
+                let currentValue = romanNumerals[input[i]];
+        
+                if (currentValue < prevValue) {
+                    result -= currentValue;
+                } else {
+                    result += currentValue;
+                }
+        
+                prevValue = currentValue;
+            }
+        
+            // Преобразование числа в строку и возвращение результата
+            return result.toString();
+        }
+
         const affiliation = extractAuthorsAndInstitutions();
     
         var title = getMetaAttributes(['meta[name="citation_title"]'], 'content');
@@ -90,8 +139,10 @@ async function extractData(page, jsonFolderPath, pdfFolderPath, siteFolderPath, 
         const publisher = getMetaAttributes(['meta[name="citation_publisher"]'], 'content');
         // const volume = getMetaAttributes(['meta[name="citation_volume"]'], 'content');
         // const issue = getMetaAttributes(['meta[name="citation_issue"]'], 'content');
-        const first_page = getMetaAttributes(['meta[name="citation_firstpage"]'], 'content');
-        const last_page = getMetaAttributes(['meta[name="citation_lastpage"]'], 'content');
+        let first_page = getMetaAttributes(['meta[name="citation_firstpage"]'], 'content');
+        first_page = romanToNumberOrReturn(first_page);
+        let last_page = getMetaAttributes(['meta[name="citation_lastpage"]'], 'content');
+        last_page = romanToNumberOrReturn(last_page);
         var language = getMetaAttributes(['meta[name="citation_language"]'], 'content');
         if (language === 'en'){
             language = 'eng';
@@ -163,7 +214,7 @@ async function extractData(page, jsonFolderPath, pdfFolderPath, siteFolderPath, 
                 if (!pdfLinks){
                     return null;
                 }
-                return pdfLinks.replace("reader", "pdf").replace("epdf", "pdf");
+                return pdfLinks.replace("reader", "pdfdirect").replace("epdf", "pdfdirect");
 
                 // const pdfLinks = Array.from(document.querySelectorAll("a[href]"))
                 // .filter(a => a.href.match(/\/doi\/reader.*/))
