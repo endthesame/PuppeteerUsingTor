eec3114ab7b1d231139b35714d289a19593dc6cb
eterentev <eterentev@tesyan.ru>
2024-03-21 15:19:06 +0300

    fix author and affil

--- crawler.js
+++ crawler.js
@@ -101,13 +101,16 @@ async function extractData(page, jsonFolderPath, pdfFolderPath, htmlFolderPath, 
         if (date.length == 4){
             date = `${date}-01-01`;
         }
-        let authors = getMetaAttributes(['meta[name="dc.Contributor"]'], 'content');
+
+        let bookAuthorsArray = Array.from(document.querySelectorAll('.book-info__authors .al-authors-list.authors .linked-name')).map(elem => elem.innerText.trim())
+        let bookAuthors = Array.from([...new Set(bookAuthorsArray)]).join('; ')
+
+        let authors = getMetaAttributes(['meta[name="citation_author"]'], 'content');
         if (authors == ""){
             let rawAuthors = Array.from(document.querySelectorAll('.wi-authors .al-authors-list .al-author-name .linked-name')).map(elem => elem.innerText.trim())
             authors = Array.from([...new Set(rawAuthors)]).join('; ')
             if (rawAuthors.length == 0){
-                authors = Array.from(document.querySelectorAll('.book-info__authors .al-authors-list.authors .linked-name')).map(elem => elem.innerText.trim())
-                authors = Array.from([...new Set(rawAuthors)]).join('; ')
+                authors = bookAuthors;
             }
         }
 
@@ -168,8 +171,24 @@ async function extractData(page, jsonFolderPath, pdfFolderPath, htmlFolderPath, 
         const type = document.querySelector('.chapterTopInfo .chapter-groups')? document.querySelector('.chapterTopInfo .chapter-groups').innerText.trim() : "";
         let editorsArray = Array.from(document.querySelectorAll('.book-info__authors .al-authors-list.editors .linked-name')).map(elem => elem.innerText.trim())
         let editors = Array.from([...new Set(editorsArray)]).join('; ')
+        let raw_editors_aff = Array.from(document.querySelectorAll('.book-info__authors .editors .al-author-name .info-card-author'))
+        .filter(elem => {
+            let author = elem.querySelector('.info-card-name')? elem.querySelector('.info-card-name').innerText.trim() : "";
+            let affilation = elem.querySelector('.aff')? elem.querySelector('.aff').innerText.trim() : "";
+            return author != "" && affilation.length != "";
+        })
+        .map(elem => {
+            let author = elem.querySelector('.info-card-name').innerText.trim();
+            let affilation = Array.from(elem.querySelectorAll('.aff > div, .aff > a'))
+                .map(block => block.textContent.trim().replace(elem.querySelector('.label')?elem.querySelector('.label').innerText : "", '')).join(" ");
+            if (affilation == ""){
+                affilation = elem.querySelector('.aff')? elem.querySelector('.aff').innerText.trim().replace(elem.querySelector('.label')?elem.querySelector('.label').innerText : "", '') : "";
+            }
+            return `${author}:${affilation}`;
+        })
+        let editors_aff = Array.from([...new Set(raw_editors_aff)]).join(";; ");
 
-        let language = getMetaAttributes(['meta[name="dc.Language"]'], 'content');
+        let language = document.querySelector('script[type="application/ld+json"]')? document.querySelector('script[type="application/ld+json"]').innerText.match(/"inLanguage":"([a-zA-Z]+)"/)? document.querySelector('script[type="application/ld+json"]').innerText.match(/"inLanguage":"([a-zA-Z]+)"/)[1] : "" : "";
         if (language == "en"){
             language = "eng";
         }
@@ -188,7 +207,7 @@ async function extractData(page, jsonFolderPath, pdfFolderPath, htmlFolderPath, 
         // }
         // const abstract = abstractTexts.join(' ') || "";
         const abstract = document.querySelector('.abstract')? document.querySelector('.abstract').innerText.trim().replaceAll("\n", " ") : "";
-        let affiliation = Array.from(document.querySelectorAll('.wi-authors .info-card-author'))
+        let raw_affiliation = Array.from(document.querySelectorAll('.wi-authors .info-card-author'))
         .filter(elem => {
             let author = elem.querySelector('.info-card-name')? elem.querySelector('.info-card-name').innerText.trim() : "";
             let affilation = elem.querySelector('.aff')? elem.querySelector('.aff').innerText.trim() : "";
@@ -203,7 +222,28 @@ async function extractData(page, jsonFolderPath, pdfFolderPath, htmlFolderPath, 
             }
             return `${author}:${affilation}`;
         })
-        .join(";; ");
+        let affiliation = Array.from([...new Set(raw_affiliation)]).join(";; ");
+
+        let raw_book_author_affiliation = Array.from(document.querySelectorAll('.wi-authors .info-card-author'))
+        .filter(elem => {
+            let author = elem.querySelector('.info-card-name')? elem.querySelector('.info-card-name').innerText.trim() : "";
+            let affilation = elem.querySelector('.aff')? elem.querySelector('.aff').innerText.trim() : "";
+            return author != "" && affilation.length != "";
+        })
+        .map(elem => {
+            let author = elem.querySelector('.info-card-name').innerText.trim();
+            let affilation = Array.from(elem.querySelectorAll('.aff > div, .aff > a'))
+                .map(block => block.textContent.trim().replace(elem.querySelector('.label')?elem.querySelector('.label').innerText : "", '')).join(" ");
+            if (affilation == ""){
+                affilation = elem.querySelector('.aff')? elem.querySelector('.aff').innerText.trim().replace(elem.querySelector('.label')?elem.querySelector('.label').innerText : "", '') : "";
+            }
+            return `${author}:${affilation}`;
+        })
+        let book_author_affiliation = Array.from([...new Set(raw_book_author_affiliation)]).join(";; ");
+
+        if (affiliation == "" && authors == bookAuthors){
+            affiliation = book_author_affiliation;
+        }
     
         // let orcids = Array.from(document.querySelectorAll('.loa .hlFld-Affiliation')).map(elem => {
         //     let authorNameElement = elem.querySelector('.loa-info-name');
@@ -219,7 +259,7 @@ async function extractData(page, jsonFolderPath, pdfFolderPath, htmlFolderPath, 
         //Type
         // const orcid = getMetaAttributes(['.orcid.ver-b'], 'href', 'a');
     
-        var metadata = { '202': title, '200': authors, '203': date, '81': abstract, '233': mf_doi, '184': mf_issn, '240': mf_isbn, '241': mf_eisbn, '201': keywords, '239': type, '235': publisher, '144': affiliation, '176': volume, '205': language, '197': first_page, '198': last_page, '243': book_series, '242': mf_book, '207': editors, '193': pages, '212': subtitle, '199': book_version};
+        var metadata = { '202': title, '200': authors, '203': date, '81': abstract, '233': mf_doi, '184': mf_issn, '240': mf_isbn, '241': mf_eisbn, '201': keywords, '239': type, '235': publisher, '144': affiliation, '176': volume, '205': language, '197': first_page, '198': last_page, '243': book_series, '242': mf_book, '207': editors, '193': pages, '212': subtitle, '199': book_version, '146': editors_aff};
         if (!title)
         {
             metadata = false
@@ -304,11 +344,11 @@ async function crawl(jsonFolderPath, pdfFolderPath, htmlFolderPath, siteFolderPa
 
             browser = await puppeteer.launch({
                 args: ['--proxy-server=127.0.0.1:8118'],
-                headless: false //'new' for "true mode" and false for "debug mode (Browser open))"
+                headless: 'new' //'new' for "true mode" and false for "debug mode (Browser open))"
             });
 
             page = await browser.newPage();
-            await page.setViewport({ width: 1600, height: 900 });
+            await page.setViewport({ width: 1920, height: 1080 });
 
             // Проверка, есть ли еще ссылки для краулинга
             let remainingLinks = fs.readFileSync(linksFilePath, 'utf-8').split('\n').filter(link => link.trim() !== '');
