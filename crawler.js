a1602333f37f91c14c772e875b3c8011bd045efe
eterentiev <endthesame@gmail.com>
2024-04-12 10:11:29 +0300

    added parsing

--- crawler.js
+++ crawler.js
@@ -10,8 +10,7 @@ const { getCurrentIP, checkAccess } = require('./utils');
 
 puppeteer.use(StealhPlugin());
 
-async function extractData(page, jsonFolderPath, pdfFolderPath, htmlFolderPath, siteFolderPath, url, downloadPDFmark = true, checkOpenAccess = true) {
-    log(`Processing URL: ${url}`);
+async function extractMetafields(page) {
     const meta_data = await page.evaluate(() => {
         const getMetaAttributes = (selectors, attribute, childSelector) => {
             let values = [];
@@ -84,7 +83,7 @@ async function extractData(page, jsonFolderPath, pdfFolderPath, htmlFolderPath, 
         if (date.length == 4){
             date = `${date}-01-01`;
         }
-        let authors = document.querySelector('#side-b p')? document.querySelector('#side-b p').innerText.trim().match(/Author\(s\):\s*(.*)/)? document.querySelector('#side-b p').innerText.trim().match(/Author\(s\):\s*(.*)/)[1].replaceAll('and', ", ") : "" : "";
+        let authors = document.querySelector('#side-b p')? document.querySelector('#side-b p').innerText.trim().match(/Author\(s\):\s*(.*)/)? document.querySelector('#side-b p').innerText.trim().match(/Author\(s\):\s*(.*)/)[1].replaceAll(' and ', ", ").replaceAll("*","") : "" : "";
         if (authors == "") {
             authors = Array.from(document.querySelectorAll('.col-md-7 p a.text-secondary')).map(elem => {
                 return elem.innerText;
@@ -132,12 +131,24 @@ async function extractData(page, jsonFolderPath, pdfFolderPath, htmlFolderPath, 
               text = "";
             }
 			return text;
-          }).join(" ").trim() || "";
+        }).join(" ").trim() || "";
+
+        const authors_aff = Array.from(document.querySelectorAll('.row .text-secondary'))
+        .filter(elem => {
+            let author = elem.innerText.replaceAll("*","") || "";
+            let affilation = elem.getAttribute("data-content").replace("<ul><li>","").replace("</li></ul>","").trim();
+            return author != "" && affilation != "";
+        })
+        .map(elem => {
+            let author = elem.innerText.replaceAll("*","");
+            let affilation = elem.getAttribute("data-content").replace("<ul><li>","").replace("</li></ul>","").trim();
+            return `${author}:${affilation}`;
+        }).join(";; ")
         
         //Type
         // const orcid = getMetaAttributes(['.orcid.ver-b'], 'href', 'a');
     
-        var metadata = { "202": title, "200": authors, "233": mf_doi, '197': first_page, '198': last_page, '81': abstract, '242': mf_book, '240': mf_isbn, '241': mf_eisbn, '203': date, '176': volume, '201': keywords, '184': mf_issn, '185': mf_eissn, '235': publisher};
+        var metadata = { "202": title, "200": authors, "233": mf_doi, '197': first_page, '198': last_page, '81': abstract, '242': mf_book, '240': mf_isbn, '241': mf_eisbn, '203': date, '176': volume, '201': keywords, '184': mf_issn, '185': mf_eissn, '235': publisher, '144':authors_aff};
         if (!title)
         {
             metadata = false
@@ -145,7 +156,13 @@ async function extractData(page, jsonFolderPath, pdfFolderPath, htmlFolderPath, 
         // log(`Data extracted from ${url}`);
         // log(`Metadata: ${JSON.stringify(metadata)}`);
         return metadata;
-    }, log);
+    });
+    return meta_data;
+}
+
+async function extractData(page, jsonFolderPath, pdfFolderPath, htmlFolderPath, siteFolderPath, url, downloadPDFmark = true, checkOpenAccess = true) {
+    log(`Processing URL: ${url}`);
+    const meta_data = await extractMetafields(page);
 
     if (!meta_data)
     {
@@ -281,4 +298,58 @@ async function crawl(jsonFolderPath, pdfFolderPath, htmlFolderPath, siteFolderPa
     log('Crawling finished.');
 }
 
-module.exports = { crawl, extractData };
+async function parsing(jsonFolderPath,  htmlFolderPath,) {
+    log('Parsing Starting.');
+    {
+        let browser;
+        let page;
+
+        try {
+            browser = await puppeteer.launch({
+                //args: ['--proxy-server=127.0.0.1:8118'],
+                headless: 'new' //'new' for "true mode" and false for "debug mode (Browser open))"
+            });
+
+            page = await browser.newPage();
+
+            const htmlFiles = fs.readdirSync(htmlFolderPath);
+            const fieldsToUpdate = ['144', '200'];
+            log(`Fields to update: ${fieldsToUpdate.join(", ")}`);
+            for (const htmlFile of htmlFiles) {
+                const htmlFilePath = path.join(htmlFolderPath, htmlFile);
+                const jsonFilePath = path.join(jsonFolderPath, htmlFile.replace('.html', '.json'));
+                if (fs.existsSync(jsonFilePath)) {
+                    const urlToHtml = `file:///${htmlFilePath}`
+                    //const urlToHtml = htmlFilePath
+                    log(`Parsing html file: ${htmlFilePath}`);
+                    await page.goto(urlToHtml, { waitUntil: 'domcontentloaded', timeout: 70000 });
+                    
+                    log(`Html loaded: ${htmlFilePath}`);
+                    const updatedData = await extractMetafields(page);
+                    log(`New data from html file: ${htmlFilePath} parsed`);
+                    const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+                    for (const key of fieldsToUpdate) {
+                        if (updatedData.hasOwnProperty(key)) {
+                            jsonData[key] = updatedData[key];
+                        }
+                    }
+                    log(`Metafields successfully updates`);
+                    fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2));
+                } else {
+                    log(`html files ${htmlFilePath}, doesnt exist json file: ${jsonFilePath}`)
+                }
+            }
+            
+        } catch (error) {
+            log(`Error during parsing: ${error.message}`);
+        } finally {
+            if (browser) {
+                await browser.close(); // Закрываем текущий браузер
+            }
+        }
+    }
+
+    log('Parsing finished.');
+}
+
+module.exports = { crawl, extractData, parsing};
