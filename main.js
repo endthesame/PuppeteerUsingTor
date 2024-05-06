13c531fe9fa28a91be2a240b023effad8e17e72b
eterentiev <endthesame@gmail.com>
2024-05-06 19:56:44 +0300

    add opt parse with links file

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ async function main() {
             .option('-oa, --open_access', 'type this if you want to check open access before download')
             .option('-t, --use_tor', 'type this if you want to use tor for crawling')
             .option('-e, --task <path>', 'path to task extractor', path.join(__dirname, 'tasks/sample_task.js'))
+            .option('-l, --links <path>', 'path to file with links', path.join(__dirname, 'your_links_file.txt'))
             .helpOption('-e, --HELP', 'read more information');
 
         program.parse();
@@ -31,6 +32,7 @@ async function main() {
         const pdfFolderPath = path.join(siteFolderPath, 'pdfs');
         const htmlFolderPath = path.join(siteFolderPath, 'htmls');
         const linksFilePath = path.join(siteFolderPath, 'remaining_links.txt');
+        const sourceLinks = program.opts().links;
 
         // Создать структуру папок, если они не существуют
         if (!fs.existsSync(outputFolderPath)) fs.mkdirSync(outputFolderPath);
@@ -39,8 +41,9 @@ async function main() {
         if (!fs.existsSync(pdfFolderPath)) fs.mkdirSync(pdfFolderPath);
         if (!fs.existsSync(htmlFolderPath)) fs.mkdirSync(htmlFolderPath);
 
+        
         // Копировать файл с ссылками
-        fs.copyFileSync('your_links_file.txt', linksFilePath);
+        fs.copyFileSync(sourceLinks, linksFilePath);
         
         // Запуск краулинга
         await crawl(jsonFolderPath, pdfFolderPath, htmlFolderPath, siteFolderPath, linksFilePath, task_path=program.opts().task, downloadPDFmark = program.opts().download_pdf, checkOpenAccess = program.opts().open_access, useTor=program.opts().use_tor);
